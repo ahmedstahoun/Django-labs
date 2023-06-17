0b0e68f4b5b00221545d6d5ec764e8c0274f328a
Ahmed Tahoun <ahmed.tahoun1997@gmail.com>
2023-06-17 19:06:27 +0300

    adding all labs

--- lab2/bookstore/books/views.py
+++ lab2/bookstore/books/views.py
@@ -6,7 +6,6 @@ from .forms import BooksForm
 
 
 def index(request):
-    print("hello")
     print(request.body)
     all_books = Books.objects.all()
     return render(request, 'book/book_list.html', context={"books": all_books})
@@ -37,16 +36,7 @@ def index(request):
 # ]
 
 
-def library_list(request):
-    my_context = {'books_list': books_list}
-    # template_loader > book/templates/
-    return render(request, 'book/book_list.html', context=my_context)
 
-def _get_book(book_id):
-    for book in books_list:
-        if 'id' in book and book['id'] == book_id:
-            return book
-    return None
 
 def create_book(request):
     return render(request, 'book/book_create.html')
@@ -67,7 +57,6 @@ def update_book(request, pk):
         if form.is_valid():
             form.save()      
             return redirect('book:book-detail',pk=book.id)
-            # return render(request, 'book:book-detail',pk=book.id)
     return render(request, 'book/book_edit.html', context={
         'form': form, 
         'book': book
